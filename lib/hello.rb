e6b9d76997bcf718a64c08eef5d87c90c69b92f8
christopher tate <chris29tate2@hotmail.co.uk>
2019-12-22 15:25:33 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,10 @@
 def hello_t((array))
-i = or
+i = 0
 
 while i < array.length
-yield array[i]
+yield(array[i]
 i = i + 1
 end
-end
 
-["Tom", "Tom, "Jim"]) do |name|
-if name.start_with? ("T)
-  puts "Hi, #{name}"
-end
+array 
 end
